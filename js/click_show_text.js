c49fb6dc36ac5ec8d5d49aa7c98c09e41e901cab
NCNB <God_bless>
2021-12-28 17:43:10 +0800

    Site updated: 2021-12-28 17:43:10

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
         ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
-        var $i = $("<span>").text(a[a_idx]);
+        var $i = $("<span></span>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
         y = e.pageY;
@@ -31,4 +31,3 @@ jQuery(document).ready(function($) {
 function delay() {
     $(".buryit").removeAttr("onclick");
 }
-</span>
